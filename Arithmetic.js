5aeae2381f8a0c435d9d9bed93678478f30b5064
MOHIT SONI 24 <152783527+8777728605@users.noreply.github.com>
2025-02-07 10:25:45 +0530

    Add files via upload

---
+++ Arithmetic.js
@@ -0,0 +1,39 @@
+// Addition
+
+var a = 100;
+var b = 100;
+
+sum =a+b;
+console.log(sum);
+ 
+// subtraction
+var c = 90;
+var d = 80;
+
+sum = c-d;
+console.log(sum);
+
+// multipication
+
+var e =100;
+var f = 1200;
+
+sum =e*f;
+console.log(sum);
+
+// division
+ 
+var g = 100;
+var h = 80;
+
+sum = g/h;
+console.log(sum);
+
+// Percentage
+var i = 200;
+var j = 100;
+
+sum = i%j;
+console.log(sum);
+
+
